05c5a2b19dcd5398099645299041ffcd3a9b56fb
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-04 14:08:22 -0500

    Added page controller to server name generator to separate functionality into a class .php file.

---
+++ php/ServerFunction.php
@@ -0,0 +1,25 @@
+<?php
+
+class ServerFunction{
+    
+    public static function generateAdjective(){
+
+        $adjective = ['adorable', 'bewildered', 'vengeful', 'relieved', 'fearless', 'cruel', 'bright', 'cooperative', 'diligent', 'glowing', 'filthy', 'scruffy', 'vivacious', 'embarrassed', 'defeated', 'brave'];
+
+        $displayedAdjective = $adjective[mt_rand(0, count($adjective) - 1)];
+
+        return $displayedAdjective;
+        }
+
+    public static function generateNoun(){
+
+        $nouns = ['chaos', 'balance', 'advantage', 'age', 'force', 'fiasco', 'celebration', 'caution', 'benefit', 'aim', 'death', 'beauty', 'comedy', 'disgrace', 'bias', 'concept', 'famine', 'humanity', 'illness'];
+
+        $displayedNoun = $nouns[mt_rand(0, count($nouns)-1)];
+
+        return $displayedNoun;
+    }
+
+}
+
+?>
--- php/server-name-generator.php
+++ php/server-name-generator.php
@@ -1,18 +1,14 @@
 <?php
 
+require_once 'ServerFunction.php';
+
 function pageController(){
 
-$adjective = ['adorable', 'bewildered', 'vengeful', 'relieved', 'fearless', 'cruel', 'bright', 'cooperative', 'diligent', 'glowing', 'filthy', 'scruffy', 'vivacious', 'embarrassed', 'defeated', 'brave'];
-
-$displayedAdjective = $adjective[mt_rand(0, count($adjective) - 1)];
-
-$nouns = ['chaos', 'balance', 'advantage', 'age', 'force', 'fiasco', 'celebration', 'caution', 'benefit', 'aim', 'death', 'beauty', 'comedy', 'disgrace', 'bias', 'concept', 'famine', 'humanity', 'illness'];
-
-$displayedNoun = $nouns[mt_rand(0, count($nouns)-1)];
-
-$printToSceen['serverName'] = $displayedAdjective . ' ' . $displayedNoun;
-
-return $printToSceen;
+$printToScreen=array();
+$adj = ServerFunction::generateAdjective();
+$noun = ServerFunction::generateNoun();
+$printToScreen['serverName']= "$adj $noun";
+return $printToScreen;
 
 }
 
@@ -28,7 +24,7 @@ extract(pageController());
 <body>
 
     <h1> Server Name Generator </h1>
-    <p> <?= $serverName ?>   </p>
+    <p> <?= $serverName ?> </p>
 
 </body>
 
